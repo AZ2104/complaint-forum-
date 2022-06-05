1e442df7ad8855beeed90ff6320854dec587b9d7
AZ2104 <72300468+AZ2104@users.noreply.github.com>
2022-06-05 11:45:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+const firebaseConfig = {
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtl3ilKsK3Z45m8IyVETQtGOnKjDUClS8",
+  authDomain: "complaint-forum-468ae.firebaseapp.com",
+  projectId: "complaint-forum-468ae",
+  storageBucket: "complaint-forum-468ae.appspot.com",
+  messagingSenderId: "1003540448129",
+  appId: "1:1003540448129:web:cdca1228d36aca7fecde80"
+};
+ 
+};
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore()
+
